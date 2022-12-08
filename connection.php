cc3b837801c1d48677a253499aaeed386f44eedb
Aloysius Butac <39267018+AloysiusButac@users.noreply.github.com>
2022-12-08 20:41:49 +0800

    Website Login adjustmets

--- connection.php
+++ connection.php
@@ -10,26 +10,21 @@
     if(!$conn) {
         die("<br/> Connection failed: ".mysqli_connect_error());
     }
-    echo "<br/>Connected succesfully.";
 
     if(isset($_POST['Login'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        echo "<br/>username: ".$username;
-        echo "<br/>password: ".$password;
-
-        $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+        $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
         $result = mysqli_query($conn, $sql);
 
         if(mysqli_num_rows($result) == 1) {
-       
-            echo "<br/>Login success!";
-            echo "<br/>username: ".$username;
-            echo "<br/>password: ".$password;
+
+            header("Location: index.html");
         }
         else {
-            echo "<br/>Login Failed";
+
+            header("Location: login.php");
         }
     }
 
--- login.php
+++ login.php
@@ -1,41 +1,65 @@
 <html>
     <head>
         <title>Test Login Form</title>
-		<style>
-            #main {
-                background: rgba(0, 255, 255,0.7);
-                border-radius: 40px;
-                margin: 10px auto 10px auto;
-                padding: 40px;
-                max-width: 50%;
-                float: center;
-                text-align: center;
-                font-family: sans-serif;
-				
-			}
-			body{
-				 background-image: url("https://www.bworldonline.com/wp-content/uploads/2022/03/Adamson-University.jpg");
-				 background-repeat: no-repeat;
-				 background-size: cover;
-				
-			}
-</style>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- BOOTSTRAP -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+        <!-- LOCAL STYLESHEETS -->
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/loginstyle.css">
+        <link rel="stylesheet" href="css/navstyle.css">
     </head>
-    <body>
-	<div id = "main">
-        <form action = "connection.php" method = "post">
-            <p>
-                <label>Username:</label>
-                <input type = "text" name = "username" />
-            </p>
-            <p>
-                <label>Password:</label>
-                <input type = "password" name = "password" />
-            </p>
-            <p>
-                <input type = "submit" name = "Login" />
-            </p>
-        </form>
-		</div>
+    <body id="login-page">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.html">
+                <img class="website-brand" src="assets/website-logo.png" alt="Tundra"/>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item mx-5"> <a class="nav-link" href="aboutus.html">ABOUT US</a> </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <img id="website-logo-big" src="assets/website-logo.png" alt="Tundra Esports">
+            </div>
+        </div>
+
+        <div class="center">
+            <h1>Login</h1>
+            <form action = "connection.php" method = "post">
+                <p class="txt_field">
+                    <label>Username</label>
+                              <span></span>
+                    <input type = "text" name = "username" />
+                </p>
+                <p class="txt_field">
+                    <label>Password</label>
+                              <span></span>
+                    <input type = "password" name = "password" />
+                </p>
+                <p>
+                    <input type = "submit" name = "Login" />
+                </p>
+
+                <div class="signup_link">
+                    Dont have an account?  <a href="register.html">Register</a>
+                </div>
+            </form>
+
+        </div>
     </body>
 </html>
